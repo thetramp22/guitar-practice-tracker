3e47288bbd03418de89e6f222c2cfa8cddb7685f
Scott Starks <esstarks@hotmail.com>
2026-05-11 11:46:15 -0600

    add practice seesion repo and service

---
+++ internal/repository/practice_session_repository.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/thetramp22/rifflog/internal/models"
+)
+
+type PracticeSessionRepository struct {
+	DB *pgx.Conn
+}
+
+func NewPracticeSessionRepository(db *pgx.Conn) *PracticeSessionRepository {
+	return &PracticeSessionRepository{DB: db}
+}
+
+func (r *PracticeSessionRepository) CreatePracticeSession(practiceSession models.PracticeSession) error {
+	query := `
+		INSERT INTO practice_sessions (skill_id, duration_minutes, notes, practiced_at, user_id)
+		VALUES ($1, $2, $3, $4, $5)
+	`
+
+	_, err := r.DB.Exec(
+		context.Background(),
+		query,
+		practiceSession.SkillID,
+		practiceSession.DurationMinutes,
+		practiceSession.Notes,
+		practiceSession.PracticedAt,
+		practiceSession.UserID,
+	)
+
+	return err
+}
---
+++ internal/services/practice_session_service.go
@@ -0,0 +1,25 @@
+package services
+
+import (
+	"github.com/thetramp22/rifflog/internal/models"
+	"github.com/thetramp22/rifflog/internal/repository"
+)
+
+type PracticeSessionService struct {
+	Repo *repository.PracticeSessionRepository
+}
+
+func NewPracticeSessionService(repo *repository.PracticeSessionRepository) *PracticeSessionService {
+	return &PracticeSessionService{Repo: repo}
+}
+
+func (s *PracticeSessionService) CreatePracticeSession(req models.SessionsRequest) error {
+	practiceSession := models.PracticeSession{
+		SkillID:         req.SkillID,
+		DurationMinutes: req.DurationMinutes,
+		PracticedAt:     req.PracticedAt,
+		Notes:           &req.Notes,
+	}
+
+	return s.Repo.CreatePracticeSession(practiceSession)
+}
